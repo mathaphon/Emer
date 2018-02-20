bc1ac2e45909db49fbbdc637b154b14083231ddc
metaphon <mataponkom60@gmail.com>
2018-02-20 12:01:41 +0700

    Add Fragment Succe

--- app/src/main/java/com/example/com_it/test/MainActivity.java
+++ app/src/main/java/com/example/com_it/test/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.com_it.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.com_it.test.fagment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+        //     Add Fragment
+        if (savedInstanceState==null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+    } // Main Method
+
+} // Main Class
